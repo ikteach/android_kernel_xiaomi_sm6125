e0cf979cc1609c7f927d1729d8a367f412219e05
akabul0us <jeremywokka@proton.me>
2026-01-29 01:17:53 -0600

    updated build script

--- build.sh
+++ build.sh
@@ -19,12 +19,13 @@ SECONDS=0 # built-in bash timer
 topdir="$(pwd)"
 tc_dir="$topdir/toolchain"
 tc_url="https://github.com/Neutron-Toolchains/clang-build-catalogue/releases/download/10032024/neutron-clang-10032024.tar.zst"
+mkdtimg_url="https://github.com/akabul0us/mkdtimg/raw/refs/heads/static/mkdtimg"
 ak3_dir="$topdir/AnyKernel3"
 
 export KBUILD_BUILD_USER="$(whoami)"
 export KBUILD_BUILD_HOST="$(hostname)"
 
-make_options='O=out ARCH=arm64 CC=clang AS=llvm-as CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- LLVM=1 LLVM_IAS=1 DTC_EXT="/usr/bin/dtc -a 64"'
+make_options='ARCH=arm64 CC=clang AS=llvm-as CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- LLVM=1 LLVM_IAS=1 DTC_EXT="/usr/bin/dtc -a 64"'
 make_prefix=""
 while getopts "m:chb" opt; do
 	case $opt in
@@ -35,11 +36,14 @@ while getopts "m:chb" opt; do
 			ak3_branch="laurel_sprout"
 			zipname="laurel-sprout-nethunter-$(date '+%Y%m%d-%H%M').zip"
 			tarball="laurel-sprout-modules-$(date '+%Y%m%d-%H%M').tar"
+			dtbo="arch/arm64/boot/dts/xiaomi/laurel_sprout-trinket-overlay.dtbo"
 		elif [ "$model" == "gingko" ]; then
 			nh_config="nethunter_gingko_defconfig"
 			ak3_branch="gingko"
 			zipname="gingko-nethunter-$(date '+%Y%m%d-%H%M').zip"
 			tarball="gingko-modules-$(date '+%Y%m%d-%H%M').tar"
+			dtbo="arch/arm64/boot/dts/xiaomi/ginkgo-trinket-overlay.dtbo"
+
 		else
 			printf "${red}Unrecognized option for -m,${clear_color}\nPlease type either ${green}laurel${clear_color} or ${green}gingko${clear_color}\n"
 			exit 1
@@ -65,7 +69,7 @@ while getopts "m:chb" opt; do
 done
 shift "$(( OPTIND - 1 ))"
 if [ -z "$model" ]; then
-	printf "${red}Missing -m option\n${clear_color}"
+	printf "${red}Missing -m flag\n${clear_color}"
 	usage
 	exit 1
 fi
@@ -77,7 +81,7 @@ confirm() {
 deny() {
 	printf "${red}no${clear_color}\n"
 }
-printf "${yellow}Checking that operating system is Linux... ${clear_color}"
+printf "Checking that operating system is ${yellow}Linux... ${clear_color}"
 if [ "$(uname -s)" == "Linux" ]; then
 	confirm
 else
@@ -85,7 +89,7 @@ else
 	printf "${red}Building not supported on operating systems other than Linux. Quitting${clear_color}\n"
 	exit 1
 fi
-printf "${yellow}Checking that architecture is x86_64... ${clear_color}"
+printf "Checking that architecture is ${yellow}x86_64... ${clear_color}"
 if [ "$(uname -m)" == "x86_64" ]; then
 	confirm
 else
@@ -94,7 +98,7 @@ else
 	exit 1
 fi
 check_installation() {
-printf "${yellow}Checking for $program... "
+printf "Checking for ${yellow}$program... "
 if command -v $program >/dev/null 2>&1; then
 	confirm
 else
@@ -115,7 +119,7 @@ printf "${green}Updating submodules${clear_color}\n"
 git submodule init
 git submodule update --recursive --remote
 if [ ! -d "$tc_dir" ]; then
-	printf "${red}Toolchain directory not found! ${yellow}Downloading to $tc_dir...${clear_color}\n"
+	printf "${red}Toolchain directory not found! ${clear_color}Downloading to ${yellow}$tc_dir...${clear_color}\n"
 	mkdir -p $tc_dir
 	cd $tc_dir
 	curl -fsSL $tc_url | tar --zstd -xvf -
@@ -127,7 +131,7 @@ if [ ! -d "$tc_dir" ]; then
 	fi
 else
 	if [ ! -e $tc_dir/bin/clang-19 ]; then
-		printf "${yellow}$tc_dir/bin does not contain expected files...\n"
+		printf "${yellow}$tc_dir/bin${clear_color} does not contain expected files...\n"
 		printf "If you want to continue anyway, perhaps using a different version of clang, type \'continue\' now: "
 		read continue_anyway
        		if [ "$continue_anyway" == "continue" ]; then
@@ -137,22 +141,31 @@ else
 			exit 1
 		fi
 	fi		
-		
+fi
+#check for mkdtimg and download if not found
+if command -v mkdtimg >/dev/null 2&>1; then
+	if [ ! -e "$tc_dir/bin/mkdtimg" ]; then
+		printf "${red}mkdtimg not found in PATH or toolchain bin directory${clear_color} - downloading...\n"
+		curl -o $tc_dir/bin/mkdtimg -fsSL $mkdtimg_url
+		chmod +x $tc_dir/bin/mkdtimg
+	else
+		printf "${yellow}mkdtimg${clear_color} found in toolchain directory\n"
+	fi
+else
+	printf "${yellow}mkdtimg${clear_color} found in user's PATH\n"
 fi
 #get anykernel3 branch
 if [ ! -d "$ak3_dir" ]; then
-	printf "${green}Cloning AnyKernel3 repo...${clear_color}\n"
+	printf "Cloning ${yellow}AnyKernel3${clear_color} repo...${clear_color}\n"
 	git clone https://github.com/akabul0us/AnyKernel3 -b $ak3_branch $ak3_dir
 else
-	printf "${yellow}AnyKernel3 repo already in source tree${clear_color}\n"
+	printf "${yellow}AnyKernel3${clear_color} repo already in source tree\n"
 fi
 export PATH="$tc_dir/bin:$PATH"
 cd $topdir
 #see if we want to clean up artifacts from a previous build
-printf "${yellow}Checking for build artifacts...\n${clear_color}"
-find . -name "*.o" > /dev/null
-found="$?"
-if [ "$found" -eq 0 ]; then
+printf "Checking for ${yellow}build artifacts...${clear_color}\n"
+if (find . -name "*.o" > /dev/null); then
         printf "${yellow}Build artifacts found -- clean before continuing?${clear_color} (y/n)\n"
         read make_clean
         if [ "$make_clean" == "y" ]; then
@@ -161,10 +174,10 @@ if [ "$found" -eq 0 ]; then
                 printf "${yellow}Continuing without cleaning${clear_color}\n"
         fi
 fi
-printf "${green}Running configuration...${clear_color}\n"
-make ${make_prefix} ${make_options} -j$(nproc --all) $config_command
-printf "\n${green}Starting compilation... ${clear_color}\n"
-make ${make_prefix} ${make_options} -j$(nproc --all)
+printf "Running ${green}configuration...${clear_color}\n"
+${make_prefix} make ${make_options} -j$(nproc --all) $config_command
+printf "\nStarting ${green}compilation... ${clear_color}\n"
+${make_prefix} make ${make_options} -j$(nproc --all)
 kernel="arch/arm64/boot/Image.gz-dtb"
 #some builds won't have the concatenated image, so allow the script to find the other option
 kernel_2="arch/arm64/boot/Image.gz"
@@ -176,26 +189,30 @@ if [ ! -f "$kernel" ]; then
 		exit 1
 	fi
 else
-	printf "${green}\nKernel compiled succesfully! ${clear_color}Zipping up...\n"
 	cd $ak3_dir
 	cp $topdir/$kernel .
+	if [ -f "$dtbo" ]; then
+	       mkdtimg create dtbo.img
+	fi
 	zip -r9 "../$zipname" * -x .git README.md *placeholder
-	printf "\nCompleted in${green} $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)! ${clear_color}\n"
 	echo "Zip: $zipname"
 fi
 #pack modules tarball
+moduledir="modules-$(date '+%Y%m%d-%H%M')"
 cd $topdir
-printf "${yellow}Looking for compiled modules...${clear_color}\n"
-module_paths="$(find . -name *.ko | sed ':a;N;$!ba;s/\n/ /g')"
-if [ -z "$module_paths" ]; then
-	printf "${yellow}None found\n${clear_color}"
-else
-	if [ ! -d "$topdir/modules" ]; then
-		mkdir -p $topdir/modules
-	fi
-	cp $module_paths $topdir/modules/
-	cd $topdir/modules
+printf "Looking for compiled ${yellow}modules...${clear_color}\n"
+if (find . -name *.ko > /dev/null); then
+	module_paths="$(find . -name *.ko)"
+	mkdir -p $topdir/$moduledir
+	for m in $module_paths; do
+		cp $m $moduledir
+	done
+	cd $moduledir
 	tar cvf ../$tarball *
 	gzip -9 ../$tarball
-	printf "${green}Modules tarball $topdir/$tarball.gz created${clear_color}\n"
+	rm -rf $moduledir
+	printf "Modules tarball ${green}$topdir/$tarball.gz${clear_color} created\n"
+else
+	printf "None found\n"
 fi
+printf "\nCompleted in${green} $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)! ${clear_color}\n"
